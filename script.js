1b5d948d1d70a120a67496e7a20d8c1cf203b058
blanknamedotcom <eoinkil1405@gmail.com>
2026-08-27 20:33:29 +0100

    Expanded the brand selection page, css is sloppy but will be repaired.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const btn = document.getElementById("btn");
+const card = document.querySelector(".manufacturer-card");
+
 btn.addEventListener("click", 
     function(){
         window.location.href = "test.html";
 });
+card.addEventListener("click", 
+    function(){
+        card.classList.toggle("flipped");
+    });
