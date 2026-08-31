97a8703a509bea283c28df91f292992ee97115aa
blanknamedotcom <eoinkil1405@gmail.com>
2026-08-31 16:33:17 +0100

    Finally got the cards to spin, the (btn) command in the JS file was blocking the spin animation, fixed that. Cards are now rounded around the corners, borders glow blue to highlight that they can be pressed.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const btn = document.getElementById("btn");
-const card = document.querySelector(".manufacturer-card");
+const cards = document.querySelectorAll(".card-inner");
 
+if(btn) { //will run on the home page, will be skipped on the other pages
 btn.addEventListener("click", 
     function(){
         window.location.href = "test.html";
 });
+}
+
+cards.forEach(function(card){  //runs the flip animation when clicked
 card.addEventListener("click", 
     function(){
-        card.classList.toggle("flipped");
+        card.classList.toggle("is-flipped");
     });
+});
