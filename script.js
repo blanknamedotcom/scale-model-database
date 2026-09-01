31b3866e8cd9bfc38aa51846141a5fe532c0fe80
blanknamedotcom <eoinkil1405@gmail.com>
2026-09-01 19:10:12 +0100

    Finally ot the button issue fixed, now you can press it without the card spinning.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("btn");
 const cards = document.querySelectorAll(".card-inner");
+const exp = document.querySelectorAll(".expl");
 
 if(btn) { //will run on the home page, will be skipped on the other pages
 btn.addEventListener("click", 
@@ -12,5 +13,13 @@ cards.forEach(function(card){  //runs the flip animation when clicked
 card.addEventListener("click", 
     function(){
         card.classList.toggle("is-flipped");
+    });
+});
+
+exp.forEach(function(explore){  //opens the scale page
+explore.addEventListener("click", 
+    function(event){
+        event.stopPropagation(); //stops the card from spinning once pressed
+        window.location.href = "scale1.html";
     });
 });
