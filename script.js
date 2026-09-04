1cb73a4b24ba07b098c6a867d748a77060259d91
blanknamedotcom <eoinkil1405@gmail.com>
2026-09-04 18:36:08 +0100

    Trying to compile every scale size Airfix has produced. Repackaged model kits by other co panies will not be included. I believe I got them all.

--- script.js
+++ script.js
@@ -23,3 +23,14 @@ explore.addEventListener("click",
         window.location.href = "scale1.html";
     });
 });
+
+const manufacturers = {
+    Airfix: {
+        scales: ["1:1", "1:8", "1:12", "1:16", "1:20", "1:24", "1:32", "1:35", "1:43", "1:48", "1:72",
+             "1:76", "1:87",  "1:100", "1:130", "1:144", "1:180", "1:350", "1:400", "1:600", "1:700", "1:1200"]
+        }
+    Tamiya: {
+        scales: [ ]
+        }
+
+    }
